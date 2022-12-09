a4d8d582f6eda357319b7a66fed851c2b358a864
Siqin Shen <61429718+siqin-shen@users.noreply.github.com>
2022-12-09 12:25:39 -0500

    Revised the sql query.

--- services/backend/src/recommendation_system/recommendation_flow/candidate_generators/UserPreferenceGenerator.py
+++ services/backend/src/recommendation_system/recommendation_flow/candidate_generators/UserPreferenceGenerator.py
@@ -8,6 +8,7 @@ from src.data_structures.approximate_nearest_neighbor import ann_with_offset
 from .AbstractGenerator import AbstractGenerator
 from .RandomGenerator import RandomGenerator
 
+
 class UserPreferenceGenerator(AbstractGenerator):
     def get_content_ids(self, user_id, limit, offset, seed, starting_point):
 
@@ -15,34 +16,72 @@ class UserPreferenceGenerator(AbstractGenerator):
             # get the preference from the user
             with db.engine.connect() as con:
                 query = """select
-                                engagement.content_id as content_id,
-                                sum(engagement.engagement_value) as engagement_time
+                                content_id
                             from
-                                (
+                            (
                                     select
-                                        distinct engagement.content_id as content_id
+                                        engagement.content_id,
+                                        sum(engagement.engagement_value)
                                     from
-                                        engagement
-                                    where
-                                        engagement.user_id = """ + str(user_id) + """
-                                        and (
-                                            engagement.engagement_type = "like"
-                                            and engagement.engagement_value = 1
-                                        )
-                                ) temp
-                                left join engagement on engagement.content_id = temp.content_id
-                            where
-                                engagement.user_id = """ + str(user_id) + """ 
-                            group by
-                                engagement.content_id
-                            having 
-                                engagement_time < 600000
+                                        (
+                                            select
+                                                content_id
+                                            from
+                                                generated_content_metadata
+                                                left join (
+                                                    select
+                                                        distinct temp1.source
+                                                    from
+                                                        (
+                                                            select
+                                                                generated_content_metadata.source
+                                                            from
+                                                                (
+                                                                    select
+                                                                        distinct engagement.content_id as content_id
+                                                                    from
+                                                                        engagement
+                                                                    where
+                                                                        engagement.user_id = """ + str(user_id) + """
+                                                                        and (
+                                                                            engagement.engagement_type = "like"
+                                                                            and engagement.engagement_value = 1
+                                                                        )
+                                                                ) temp
+                                                                left join engagement on engagement.content_id = temp.content_id
+                                                                left join generated_content_metadata on engagement.content_id = generated_content_metadata.content_id
+                                                            group by
+                                                                engagement.content_id,
+                                                                generated_content_metadata.source
+                                                            having
+                                                                sum(engagement.engagement_value) < 600000
+                                                            order by
+                                                                sum(engagement.engagement_value) desc
+                                                            limit
+                                                                10
+                                                        ) temp1
+                                                    order by
+                                                        RAND ()
+                                                    limit
+                                                        1
+                                                ) temp2 on temp2.source = generated_content_metadata.source
+                                        ) temp3
+                                        left join engagement on engagement.content_id = temp3.content_id
+                                    group by
+                                        engagement.content_id
+                                    having
+                                        sum(engagement.engagement_value) < 600000
+                                        and sum(engagement.engagement_value) > 6000
+                                    order by
+                                        sum(engagement.engagement_value) desc
+                                ) temp4
                             order by
-                                engagement_time desc
-                            limit """ + str(limit) + """;
+                                RAND ()
+                            limit
+                                """ + str(limit) + """;
                             """
 
-            results = con.execute(query).all()
+                results = con.execute(query).all()
 
             num_results = len(results)
 
@@ -51,29 +90,11 @@ class UserPreferenceGenerator(AbstractGenerator):
                 return RandomGenerator().get_content_ids(
                     user_id, limit, offset, seed, starting_point
                 )
-                
-            # get 2x so we can take the best
-            new_limit = 2 * (limit // num_results + 1)
-            new_offset = offset // num_results + 1
-            new_result, new_scores = [], []
-            for (content_id, ms) in results:
-                # can probably do this faster as a heap so instead of `2 * limit * (1+log2(limit))` in `limit * log2(limit)`
-                content_ids, scores = ann_with_offset(
-                    content_id, 0.9, 2 * new_limit, new_offset, return_distances=True
-                )
-                new_result.extend(content_ids)
-                # TODO: score and ms should probably be normalized so multiplication makes sense
-                new_scores.extend(map(lambda score: score * ms, scores))
-            if len(new_result) == 0:
-                return RandomGenerator().get_content_ids(
-                    user_id, limit, offset, seed, starting_point
-                )
-            results_with_scores = sorted(
-                list(zip(new_result, new_scores)), key=operator.itemgetter(1)
-            )[:limit]
-            return list(map(operator.itemgetter(0), results_with_scores)), list(
-                map(operator.itemgetter(1), results_with_scores)
-            )
+
+            ids = list(map(lambda x: x[0], results))
+
+            return ids, None
+
         elif starting_point.get("content_id", False):
             content_ids, scores = ann_with_offset(
                 starting_point["content_id"], 0.9, limit, offset, return_distances=True
