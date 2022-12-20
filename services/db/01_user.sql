92368e1e9edfd4fca5e2574cb0ac5eafca259707
Monica Guntur <75153702+monicaguntur@users.noreply.github.com>
2022-12-20 18:57:15 -0500

    updated password for dummy user to password

--- services/db/01_user.sql
+++ services/db/01_user.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a5aa79aec85e2fbb7f58240b4e3f90b7a0a7e42d9017196053a48f928a3c01a
-size 9757
+oid sha256:344d3baa853c99fc699cf9f433141a26ee20b5322eb86e807584ca2aa5aab3c2
+size 9758
